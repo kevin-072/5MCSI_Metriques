b54a3bc9a9fbbd5deb71b5099b5630ef668fdc89
kevin-072 <189124400+kevin-072@users.noreply.github.com>
2024-11-20 17:54:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,12 +10,6 @@ app = Flask(__name__)
 from flask import Flask, render_template_string, request
 
 app = Flask(__name__)
-
-@app.route('/extract-minutes/<date_string>')
-def extract_minutes(date_string):
-        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-        minutes = date_object.minute
-        return jsonify({'minutes': minutes})
   
 @app.route("/contact/", methods=["GET", "POST"])
 def MaPremiereAPI():
@@ -115,6 +109,10 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/commits/")
+def commits():
+    return render_template("commits.html")
   
 @app.route("/histogramme/")
 def monhistogramme():
